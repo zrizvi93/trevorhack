b549c92a68e807c284204074b38537a6a069343a
zrizvi93 <zrizvi@atlassian.com>
2024-02-03 13:34:30 -0800

    initial chat interface

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
-# trevorhack
+# 🦙📚 LlamaIndex - Chat with the Streamlit docs
+
+Build a chatbot powered by LlamaIndex that augments GPT 3.5 with the contents of the Streamlit docs (or your own data).
+
+## Local Dev Loop
+
+`pyenv local`
+Use python version in .python-version file
+
+
+`virtualenv -v venv-trevorhack-streamlit && source venv-trevorhack-streamlit/bin/activate`
+Create and activate virtual environment
+
+
+`mkdir .streamlit && cd .streamlit && touch secrets.toml`
+Create secrets file 
+
+
+openai_key = "XXXX"
+insert your API key for open ai / others like so
+
+
+`pip install -r requirements.txt`
+Install deps
+
+
+`streamlit run streamlit_app.py`
+Run app from the project root
+
+
+The dev server can be accessed at [localhost:8501](http://localhost:8501/)
+
+
+Additional Notes:
+
+`kill -9 $(lsof -i:8501 -t) 2> /dev/null`
+To quickly kill any process running on port 8501
+
+
+## Overview of the App
+
+<img src="app.png" width="75%">
+
+- Takes user queries via Streamlit's `st.chat_input` and displays both user queries and model responses with `st.chat_message`
+- Uses LlamaIndex to load and index data and create a chat engine that will retrieve context from that data to respond to each user query
+
+## Demo App
+
+[![Streamlit App](https://static.streamlit.io/badges/streamlit_badge_black_white.svg)](https://llamaindex-chat-with-docs.streamlit.app/)
+
+## Get an OpenAI API key
+
+You can get your own OpenAI API key by following the following instructions:
+1. Go to https://platform.openai.com/account/api-keys.
+2. Click on the `+ Create new secret key` button.
+3. Next, enter an identifier name (optional) and click on the `Create secret key` button.
+
+## Try out the app
+
+Once the app is loaded, enter your question about the Streamlit library and wait for a response.
---
+++ streamlit_app.py
@@ -0,0 +1,45 @@
+import streamlit as st
+from llama_index import VectorStoreIndex, ServiceContext, Document
+from llama_index.llms import OpenAI
+import openai
+from llama_index import SimpleDirectoryReader
+
+st.set_page_config(page_title="Chat with the Streamlit docs, powered by LlamaIndex", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
+openai.api_key = st.secrets.openai_key
+st.title("Chat with the Streamlit docs, powered by LlamaIndex 💬🦙")
+st.info("Check out the full tutorial to build this app in our [blog post](https://blog.streamlit.io/build-a-chatbot-with-custom-data-sources-powered-by-llamaindex/)", icon="📃")
+         
+if "messages" not in st.session_state.keys(): # Initialize the chat messages history
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Ask me a question about Streamlit's open-source Python library!"}
+    ]
+
+@st.cache_resource(show_spinner=False)
+def load_data():
+    with st.spinner(text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."):
+        reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
+        docs = reader.load_data()
+        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features."))
+        index = VectorStoreIndex.from_documents(docs, service_context=service_context)
+        return index
+
+index = load_data()
+
+if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
+        st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
+
+if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+for message in st.session_state.messages: # Display the prior chat messages
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+# If last message is not from assistant, generate a new response
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = st.session_state.chat_engine.chat(prompt)
+            st.write(response.response)
+            message = {"role": "assistant", "content": response.response}
+            st.session_state.messages.append(message) # Add response to message history
