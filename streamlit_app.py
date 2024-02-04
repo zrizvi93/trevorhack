a00c407ebcd338a5fde452d57213ed284a3e75c6
zrizvi93 <zrizvi@atlassian.com>
2024-02-03 20:21:26 -0800

    resolved merge conflicts again

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,15 +8,10 @@ import time
 # Client conversation idx initialization
 if 'script_idx' not in st.session_state:
     st.session_state.script_idx = 1
-    
+
 st.set_page_config(page_title="Chat with the Streamlit docs, powered by LlamaIndex", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
 openai.api_key = st.secrets.openai_key
-st.title("Welcome to TrevorText, powered by LlamaIndex 💬🦙")
-
-if "messages" not in st.session_state.keys(): # Initialize the chat messages history
-    st.session_state.messages = [
-        {"role": "user", "content": "Hi, welcome to TrevorText. What's going on?"}
-    ]
+st.title("Welcome to TrevorText, powered by LlamaIndex 💬🦙⚠️")
 
 @st.cache_resource(show_spinner=False)
 def load_data():
@@ -29,35 +24,59 @@ def load_data():
 
 index = load_data()
 
-col1, col2, col3 = st.columns([0.4, 0.3, 0.3], gap="small")
+tab1, tab2 = st.tabs(["Crisis Hotline Chat", "Case Form"])
 
-with col1:
-    if "chat_engine" not in st.session_state.keys():   # Initialize the chat engine
-        st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
-    if prompt := st.chat_input("Your question"):   # Prompt for user input and save to chat history
-        st.session_state.messages.append({"role": "user", "content": prompt})
+# crisis intervention room tab
+with tab1:
+    col_a1, col_a2 = st.columns([0.5, 0.5], gap="small")
+    with col_a1:
+        if "messages" not in st.session_state.keys(): # Initialize the chat messages history
+            st.session_state.messages = [
+                {"role": "assistant", "content": "Ask me a question about Streamlit's open-source Python library!"}
+            ]
 
-with col2:
-    with st.container(height=570):
-        age = st.number_input("Age", value=0)
-        city = st.text_input("City")
-        state = st.text_input("State")
-        suicidal_bool = st.checkbox("Are they thinking of killing themselves?")
-        summary = st.text_area("Brief summary/ Narrative")
-        risk_level = st.selectbox("Risk Level", ["Not Suicidal", "Low Risk", "Medium Risk", "High Risk", "Imminent Risk"])
-    st.subheader("Reply Suggestions")
+        for message in st.session_state.messages:   # Display the prior chat messages
+            with st.chat_message(message["role"]):
+                st.write(message["content"])
+    
+        if "chat_engine" not in st.session_state.keys():   # Initialize the chat engine
+            st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
+        if prompt := st.chat_input("Your question"):   # Prompt for user input and save to chat history
+            st.session_state.messages.append({"role": "user", "content": prompt})
 
+        # If last message is not from assistant, generate a new response
+        if st.session_state.messages[-1]["role"] != "assistant":
+            with st.chat_message("assistant"):
+                with st.spinner("Thinking..."):
+                    response = st.session_state.chat_engine.chat(prompt)
+                    st.write(response.response)
+                    message = {"role": "assistant", "content": response.response}
+                    st.session_state.messages.append(message)  # Add response to message history
+        with col_a2:
+            st.subheader("Suggested Reply")
+            st.info("Have you been feeling overwhelmed or hopeless?")
+            st.subheader("Sources")
 
-with col3:
-    with st.container(height=190):
-        name = st.text_input("First Name")
-        primary_issue = st.text_input("Primary Issue")
+            source_file_names = ["README.md", "HelloWorld.py", "GirlPowerPlusTarun.pdf"]
+            source_links = []
+            base_link = "https://github.com/zrizvi93/trevorhack/tree/main/data/{}"
+            for file in source_file_names:
+                source_links.append(base_link.format(file))
 
-for message in st.session_state.messages: # Display the prior chat messages
-    with st.chat_message(message["role"]):
-        st.write(message["content"])
+            i = 0
+            sources_row = st.columns(3)
+            for col in sources_row:
+                with col.container(height=50):
+                    st.markdown(f'<a href="{source_links[i]}" target="_blank">"{source_file_names[i]}"</a>', unsafe_allow_html=True)
+                i += 1
 
-client_script = open("data/library/demo_conversation_client.txt", "r").readlines()
+            st.subheader("Additional Research")
+            st.write('''
+            <div style="width: 100%; padding-top: 70%; position: relative;">
+                <iframe src="https://www.perplexity.ai/" style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"></iframe>
+            </div>''', unsafe_allow_html=True)
+
+    client_script = open("data/library/demo_conversation_client.txt", "r").readlines()
 
 # If last message is not from assistant, generate a new response
 if st.session_state.messages[-1]["role"] != "assistant":
@@ -71,4 +90,3 @@ if st.session_state.messages[-1]["role"] != "assistant":
             st.session_state.messages.append(message) # Add response to message history
         else:
             st.info("Contact has left the chat")
-        
