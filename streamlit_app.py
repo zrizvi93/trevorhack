eef323c39d9800c13a58ddc64f9ae88f172a3039
apiyapan <apiyapan@caltech.edu>
2024-02-03 20:04:51 -0800

    chat sleep

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ from llama_index import VectorStoreIndex, ServiceContext, Document
 from llama_index.llms import OpenAI
 import openai
 from llama_index import SimpleDirectoryReader
+import time
 
 # Client conversation idx initialization
 if 'script_idx' not in st.session_state:
@@ -10,8 +11,7 @@ if 'script_idx' not in st.session_state:
     
 st.set_page_config(page_title="Chat with the Streamlit docs, powered by LlamaIndex", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
 openai.api_key = st.secrets.openai_key
-st.title("TrevorText")
-# st.info("Check out the full tutorial to build this app in our [blog post](https://blog.streamlit.io/build-a-chatbot-with-custom-data-sources-powered-by-llamaindex/)", icon="📃")
+st.title("Welcome to TrevorText, powered by LlamaIndex 💬🦙")
 
 if "messages" not in st.session_state.keys(): # Initialize the chat messages history
     st.session_state.messages = [
@@ -20,7 +20,7 @@ if "messages" not in st.session_state.keys(): # Initialize the chat messages his
 
 @st.cache_resource(show_spinner=False)
 def load_data():
-    with st.spinner(text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."):
+    with st.spinner(text="Loading"):
         reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
         docs = reader.load_data()
         service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features."))
@@ -62,6 +62,7 @@ client_script = open("data/library/demo_conversation_client.txt", "r").readlines
 
 # If last message is not from assistant, generate a new response
 if st.session_state.messages[-1]["role"] != "assistant":
+    time.sleep(2) 
     with st.chat_message("assistant"):
         response = "sorry"
         if st.session_state.script_idx < len(client_script):
